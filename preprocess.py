3c293fa22ddb08dc312d146eb6831024b27e0fba
someone <someone@example.com>
2018-10-30 22:53:20 +0900

    Change directory structure of preprocess out

--- preprocess.py
+++ preprocess.py
@@ -9,20 +9,27 @@ from tqdm import tqdm
 from util import *
 
 flags.DEFINE_string("input", "../input/train_simplified", "path to input directory", short_name="i")
-flags.DEFINE_string('extract', '../work/train_simplified', 'path to extracted input')
+flags.DEFINE_string('output', '../work/train_simplified', 'path to extracted input')
 
 FLAGS = flags.FLAGS
 
-def main(argv=None):
-    csv_files = sorted(glob.glob(os.path.join(FLAGS.input, "*.csv")))
 
-    os.makedirs(FLAGS.extract, exist_ok=True)
+def main(argv=None):
+    if os.path.isdir(FLAGS.input):
+        csv_files = sorted(glob.glob(os.path.join(FLAGS.input, "*.csv")))
+    elif os.path.isfile(FLAGS.input) and FLAGS.input.endswith('.csv'):
+        csv_files = [FLAGS.input]
+    else:
+        raise ValueError("input must be directory or csv file")
+
+    os.makedirs(FLAGS.output, exist_ok=True)
+    os.makedirs(os.path.join(FLAGS.output, 'csv'), exist_ok=True)
 
     for i, f in enumerate(csv_files):
         print("Loading {}/{} {}".format(i+1, len(csv_files), f))
         df = load_csv(f)
-        path_npy = os.path.join(FLAGS.extract, 'drawing')
-        path_csv = os.path.join(FLAGS.extract, os.path.basename(f))
+        path_npy = os.path.join(FLAGS.output, 'drawing', os.path.splitext(os.path.basename(f))[0])
+        path_csv = os.path.join(FLAGS.output, 'csv', os.path.basename(f))
         save_csv_and_drawing(df, path_csv, path_npy)
 
 
@@ -34,6 +41,7 @@ def load_csv(csv_file):
 
 def save_csv_and_drawing(df, path_csv, path_npy):
     os.makedirs(path_npy, exist_ok=True)
+
     print("Saving drawing")
     for index, row in tqdm(df.iterrows(), total=len(df.index)):
         drawing = row['drawing']
